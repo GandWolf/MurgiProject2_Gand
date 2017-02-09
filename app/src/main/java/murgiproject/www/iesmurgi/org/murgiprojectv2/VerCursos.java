7ae20835395af470ecaa3a055809bd1d4d75998f
mjodar <jodarsanchezmiguel@gmail.com>
2017-02-09 11:01:11 +0100

    limpieza  09/02/17

--- app/src/main/java/murgiproject/www/iesmurgi/org/murgiprojectv2/VerCursos.java
+++ app/src/main/java/murgiproject/www/iesmurgi/org/murgiprojectv2/VerCursos.java
@@ -3,8 +3,6 @@ package murgiproject.www.iesmurgi.org.murgiprojectv2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.View;
 import android.widget.ListView;
 
